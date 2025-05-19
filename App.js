702d5fcbe5e146f689bf90e5e7fa2aed66917229
Antonlvanov <156616115+Antonlvanov@users.noreply.github.com>
2025-05-19 13:20:28 +0300

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hochu poehat na gavai, no budu sidet doma');
 });
 
 app.listen(port, () => {
